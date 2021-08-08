bb0bc9bb072edc890fb4eea098b26343161d3d27
Erin Moon <erin@hecke.rs>
2021-08-08 14:17:39 -0400

    refactor with macros

--- boopsector.s
+++ boopsector.s
@@ -8,20 +8,44 @@
 	%define t 0x7e01
 	%define i 0x7e02
 
+	;; macros
+%macro cursor_move 0
+	mov ah, 0x02	; ah = 0x2 : set cursor position
+	int 0x10		; set cursor pos using dx
+%endmacro
+
+%macro screen_clear 0
+	; -- clear screen by changing mode
+	mov ah, 0x0
+	mov al, 0x3		; 0x3 = text, 80x25, 16color
+	int 0x10
+%endmacro
+
+%macro cursor_off 0
+	mov ah, 0x01
+	mov ch, 0x3f
+	int 0x10		; disable cursor
+%endmacro
+
+%macro cpos_bump 1
+	add dl, %1		; increment col by number of chars we 'wrote'
+	cmp dl, 61		; is our col past end?
+	jl %%no_row_incr; jump over if we don't need to update
+	mov dl, 0 		; reset col
+	add dh, 1		; increment row
+%%no_row_incr:
+%endmacro
+
 	;; start doing shit
 	xor ax, ax		; ax = 0
 	mov ds, ax		; ds = ax = 0
 	xor cx, cx
-	mov sp, 0
 
 	; -- start at 0,0
 	mov dx, 0		; dh : row, dl : col
 
-	call cls		; clear screen
-
-	mov ah, 0x01
-	mov ch, 0x3f
-	int 0x10		; disable cursor
+	screen_clear
+	cursor_off
 
 	mov byte [t], 0
 
@@ -42,6 +66,8 @@ unpack:
 
 	mov cx, 1
 .printloop:
+	cursor_move
+
 	mov ah, 0x9			; ah = 0x9 : write char w attr at cursor
 	mov bh, 0x0 		; page = 0
 
@@ -51,7 +77,7 @@ unpack:
 	add bl, 1
 
 	int 0x10
-	call bump_cursor
+	cpos_bump 1
 	dec byte [i]
 	jnz .printloop
 	jmp load
@@ -62,8 +88,6 @@ load:
 	jnz unpack		; if we're not done yet, loop
 
 	mov dx, 0
-	mov cl, 0
-	call bump_cursor
 
 	add byte [t], 1
 	mov si, woof
@@ -72,29 +96,9 @@ load:
 ; hang:
 ; 	jmp hang
 
-cls:
-	; -- clear screen by changing mode
-	mov ah, 0x0
-	mov al, 0x3		; 0x3 = text, 80x25, 16color
-	int 0x10
-	ret
-
-;;; takes cx as param
-;;; clobbers dx, ah
-bump_cursor:
-	add dl, cl		; increment col by number of chars we 'wrote'
-	cmp dl, 61		; is our col past end?
-	jl .no_row_incr		; jump over if we don't need to update
-	mov dl, 0 		; reset col
-	add dh, 1		; increment row
-.no_row_incr:
-	mov ah, 0x02		; ah = 0x2 : set cursor position
-	int 0x10		; set cursor pos using dx
-	ret
-
 woof:
 	incbin "rle.dat"
 	db 0x0 ; end marker
 
 	times 510-($-$$) db 0	; pad til end of sector
-	dw 0xaa55		; magic
+; 	dw 0xaa55		; magic
